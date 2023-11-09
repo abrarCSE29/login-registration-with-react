ae0c99a258ee13e1f91037b77898d3e4ddfafb3d
abrarCSE29 <jucse29.398@gmail.com>
2023-11-09 16:56:07 +0600

    Create footer

---
+++ src/COMPONENTS/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="container footer fontstyle-bold padding-5">
+      <div>
+        <p className="color-orange  margin-25">Developed and maintained by</p>
+        <p>Department of Computer Science and Engineering<br/>
+        Jahangirnar University, Savar, Dhaka</p>
+      </div>
+      <div>
+        <p className="color-orange  margin-25 ">Contact :</p>
+        <p>Phone : 01521102764<br/>
+        Email : admin@juniv.edu </p>
+      </div>
+    </div>
+  );
+}
