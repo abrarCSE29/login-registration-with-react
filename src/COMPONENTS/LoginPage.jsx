34016baa3d7731fd22f5b70a8159aa646da2c6fe
abrarCSE29 <jucse29.398@gmail.com>
2023-11-10 09:50:34 +0600

    Update work

--- src/COMPONENTS/LoginPage.jsx
+++ src/COMPONENTS/LoginPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TopNavBar from './TopNavBar'
 import Footer from './Footer'
+import LoginCard from './LoginCard'
 
 export default function LoginPage() {
   return (
     <div>
       <TopNavBar/>
+      <LoginCard/>
       <Footer/>
     </div>
   )
